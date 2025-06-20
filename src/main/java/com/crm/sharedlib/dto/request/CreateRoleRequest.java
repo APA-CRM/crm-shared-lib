06d41fb86a9e3686ab1679c5879f6d1048d16c86
Pavel Tseva <paveltseva@gmail.com>
2025-06-20 20:46:57 +0300

    Release 0.0.6-SNAPSHOT
    Separate DTOs for creation and update of role

---
+++ src/main/java/com/crm/sharedlib/dto/request/CreateRoleRequest.java
@@ -0,0 +1,18 @@
+package com.crm.sharedlib.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateRoleRequest extends RoleRequest {
+
+    @NotNull(message = "isDeletable field can't be null")
+    private Boolean isDeletable;
+
+}
