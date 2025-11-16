91329569676c7ddff4c8838e413a528f969f9f4c
Pavel Tseva <paveltseva@gmail.com>
2025-11-16 18:27:15 +0200

    Change Queues names related to sending emails

--- src/main/java/com/crm/sharedlib/consts/CrmConstants.java
+++ src/main/java/com/crm/sharedlib/consts/CrmConstants.java
@@ -8,12 +8,12 @@ public class CrmConstants {
 
     public final static String ORGANIZATION_ID_HEADER_NAME = "Organization-Id";
 
-    public final static String SEND_PASSWORD_QUEUE = "main-service.send-password";
+    public final static String SEND_PASSWORD_QUEUE = "notification-service.send-password";
 
-    public final static String SEND_WELCOME_EMAIL_QUEUE = "main-service.send-welcome-email";
+    public final static String SEND_WELCOME_EMAIL_QUEUE = "notification-service.send-welcome-email";
 
     public final static String ORGANIZATION_USER_ROLES_SYNC_QUEUE = "auth-service.org-user-role-sync";
 
-    public final static String SEND_INVITATION_OF_ORGANIZATION = "main-service.send-invitation-of-organization";
+    public final static String SEND_INVITATION_OF_ORGANIZATION = "notification-service.send-invitation-of-organization";
 
 }
