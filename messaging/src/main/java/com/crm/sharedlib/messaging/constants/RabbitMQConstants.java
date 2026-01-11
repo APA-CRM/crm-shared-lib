6362ec1c2484c98b2892eb3cc2d2eb148af4805e
Pavel Tseva <paveltseva@gmail.com>
2026-01-11 21:41:37 +0200

    Add SEND_VERIFICATION_CODE_QUEUE content in RabbitMQConstants

--- messaging/src/main/java/com/crm/sharedlib/messaging/constants/RabbitMQConstants.java
+++ messaging/src/main/java/com/crm/sharedlib/messaging/constants/RabbitMQConstants.java
@@ -4,6 +4,8 @@ public class RabbitMQConstants {
 
     public final static String SEND_PASSWORD_QUEUE = "notification-service.send-password";
 
+    public final static String SEND_VERIFICATION_CODE_QUEUE = "notification-service.send-verification-code";
+
     public final static String SEND_WELCOME_EMAIL_QUEUE = "notification-service.send-welcome-email";
 
     public final static String ORGANIZATION_USER_ROLES_SYNC_QUEUE = "auth-service.org-user-role-sync";
