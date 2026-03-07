055b13b0850eb410a7ac27c13e8dfe2f78efe068
Pavel Tseva <paveltseva@gmail.com>
2026-03-07 15:02:51 +0200

    Introduce rbac module

---
+++ rbac/src/main/java/com/crm/sharedlib/rbac/constants/CacheConstants.java
@@ -0,0 +1,7 @@
+package com.crm.sharedlib.rbac.constants;
+
+public class CacheConstants {
+
+    public static final String USER_PERMISSION_FORMAT_KEY = "organization:%d:user:%d:permissions";
+
+}
---
+++ rbac/src/main/java/com/crm/sharedlib/rbac/constants/JwtConstants.java
@@ -0,0 +1,9 @@
+package com.crm.sharedlib.rbac.constants;
+
+public class JwtConstants {
+
+    public static final String USER_ID_KEY = "userId";
+
+    public static final String USER_LOGIN_KEY = "login";
+
+}
